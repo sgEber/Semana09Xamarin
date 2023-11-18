92333b6e857103b33cfcfdcb71add1d6e484889a
esHakomo <eber.solano@tecsup.edu.pe>
2023-11-18 11:11:26 -0500

    SwipedDemo

--- Semana09Xamarin/Semana09Xamarin/MainPage.xaml.cs
+++ Semana09Xamarin/Semana09Xamarin/MainPage.xaml.cs
@@ -14,5 +14,25 @@ namespace Semana09Xamarin
         {
             InitializeComponent();
         }
+
+        private void OnTapDemoClicked(object sender, EventArgs e)
+        {
+            // Navega a la página de demo de pulsación
+        }
+
+        private void OnSwipeDemoClicked(object sender, EventArgs e)
+        {
+            // Navega a la página de demo de deslizamiento
+        }
+
+        private void OnPinchDemoClicked(object sender, EventArgs e)
+        {
+            // Navega a la página de demo de pellizco
+        }
+
+        private void OnPanDemoClicked(object sender, EventArgs e)
+        {
+            // Navega a la página de demo de paneo
+        }
     }
 }
